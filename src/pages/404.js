70d225bc73d60eb4416f712d5e09019301d892e3
Sebastian Fehrnström-Gröhn <sebastian@greenbyte.com>
2022-02-06 12:36:01 +0100

    Clean up template pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,51 +1,27 @@
 import { Link } from "gatsby";
 import * as React from "react";
 
-// styles
 const pageStyles = {
+  width: "100vw",
+  height: "100vh",
+  padding: 96,
+
+  background: "#f0f0f0",
   color: "#232129",
-  padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
+
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  fontSize: 40,
 };
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
-// markup
 const NotFoundPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+      <title>Sidan finns inte – 🌿 Fehrnström-Gröhn</title>
+      <h1 style={headingStyles}>Sidan finns inte</h1>
+      <p>
+        <Link to="/">Gå till startsidan</Link>
       </p>
     </main>
   );
