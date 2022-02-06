6fb75b4f04dcd0151a51c8302136fb648beecb86
Sebastian Fehrnström-Gröhn <sebastian@greenbyte.com>
2022-02-06 21:44:27 +0100

    First iteration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,85 @@
 import * as React from "react";
 
-const pageStyles = {
-  width: "100vw",
-  height: "100vh",
-  padding: 96,
-
-  background: "#f0f0f0",
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-
-const headingStyles = {
-  fontSize: 40,
-};
-
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>🌿 Fehrnström-Gröhn</title>
-      <h1 style={headingStyles}>🌿 Fehrnström-Gröhn</h1>
-    </main>
-  );
-};
+const IndexPage = () => (
+  <main
+    className="
+        w-full min-h-screen
+        bg-teal-975 text-teal-50
+        flex flex-col items-center justify-evenly
+        "
+  >
+    <title>🌿 Fehrnström-Gröhn</title>
+    <div
+      className="
+        px-8 md:px-16 py-4 md:py-8
+        flex flex-col items-center gap-8 md:gap-16 lg:gap-32
+      "
+    >
+      <span className="text-5xl md:text-6xl lg:text-7xl">🌿</span>
+      <h1 className="text-4xl md:text-5xl lg:text-6xl">Fehrnström-Gröhn</h1>
+    </div>
+    <div
+      className="
+        w-full lg:w-5/6 px-8 md:px-16 py-4 md:py-8
+        flex flex-row justify-evenly gap-8 md:gap-16
+        text-base md:text-lg lg:text-xl
+      "
+    >
+      <div
+        className="
+          basis-1/3
+          flex flex-col gap-4 md:gap-8
+        "
+      >
+        <p>
+          <a
+            className="underline underline-offset-4 decoration-blue-300"
+            href="mailto:%22Linnea Fehrnström-Gröhn <linnea@fehrnstrom-grohn.se>%22"
+          >
+            Linnéa
+          </a>
+        </p>
+        <p>
+          <a
+            className="underline underline-offset-4 decoration-blue-300"
+            href="https://www.fehrnstrom-ark.se/"
+          >
+            Fehrnström Arkitektur
+          </a>
+        </p>
+      </div>
+      <div
+        className="
+          basis-1/3
+          flex flex-col gap-4 md:gap-8
+        "
+      >
+        <p>
+          <a
+            className="underline underline-offset-4 decoration-blue-300"
+            href="mailto:hus@fehrnstrom-grohn.se"
+          >
+            Hus
+          </a>
+        </p>
+      </div>
+      <div
+        className="
+          basis-1/3
+          flex flex-col gap-4 md:gap-8
+        "
+      >
+        <p>
+          <a
+            className="underline underline-offset-4 decoration-blue-300"
+            href="mailto:%22Sebastian Fehrnström-Gröhn <sebastian@fehrnstrom-grohn.se>%22"
+          >
+            Sebastian
+          </a>
+        </p>
+      </div>
+    </div>
+  </main>
+);
 
 export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,57 @@
+// https://tailwindcss.com/docs/customizing-colors
+// https://palettte.app/
+const colorPalettes = [
+  {
+    paletteName: "blue",
+    swatches: [
+      { name: "50", color: "EFF6FF" },
+      { name: "100", color: "DBEAFE" },
+      { name: "200", color: "BFDBFE" },
+      { name: "300", color: "93C5FD" },
+      { name: "400", color: "60A5FA" },
+      { name: "500", color: "3B82F6" },
+      { name: "600", color: "2563EB" },
+      { name: "700", color: "1D4ED8" },
+      { name: "800", color: "1E40AF" },
+      { name: "900", color: "1E3A8A" },
+      { name: "950", color: "0E1C41" },
+      { name: "975", color: "070D1F" },
+      { name: "990", color: "03050C" },
+    ],
+  },
+  {
+    paletteName: "teal",
+    swatches: [
+      { name: "50", color: "F0FDFA" },
+      { name: "100", color: "CCFBF1" },
+      { name: "200", color: "99F6E4" },
+      { name: "300", color: "5EEAD4" },
+      { name: "400", color: "2DD4BF" },
+      { name: "500", color: "14B8A6" },
+      { name: "600", color: "0D9488" },
+      { name: "700", color: "0F766E" },
+      { name: "800", color: "115E59" },
+      { name: "900", color: "134E4A" },
+      { name: "950", color: "0A2A28" },
+      { name: "975", color: "051514" },
+      { name: "990", color: "020A09" },
+    ],
+  },
+];
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: Object.fromEntries(
+        colorPalettes.map(({ paletteName, swatches }) => [
+          paletteName,
+          Object.fromEntries(
+            swatches.map(({ name, color }) => [name, `#${color}`])
+          ),
+        ])
+      ),
+    },
   },
   plugins: [],
 };
